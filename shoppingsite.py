62ed4befd4f14feea2c1cf77fed8340fb61c4186
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-18 17:39:49 -0700

    Bug fixes, starting to implement shopping cart functionality

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,10 +6,11 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
+
 
 app = Flask(__name__)
 
@@ -48,7 +49,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id)
     print(melon)
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -98,7 +99,16 @@ def add_to_cart(melon_id):
     # - flash a success message
     # - redirect the user to the cart page
 
-    return "Oops! This needs to be implemented!"
+    
+    if "cart" in session:
+        print("cart")
+
+    # session["cart"] = session["cart"].append(melon_id)
+    # print(session["cart"])
+
+    
+
+    return redirect("/cart")
 
 
 @app.route("/login", methods=["GET"])
